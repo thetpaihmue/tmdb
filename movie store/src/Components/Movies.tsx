8e2b3ac722ba941ce132fd25c86a6966e9b094a9
Thet Paing Hmue <thetpainghmue@cmhl.com.mm>
2023-10-16 17:31:36 +0630

    movie list done

--- movie store/src/Components/Movies.tsx
+++ movie store/src/Components/Movies.tsx
@@ -2,29 +2,50 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "../Styles/Movies.css";
 import { useEffect, useState } from "react";
 import { popularMovies } from "../Services/Apis";
+import { Button, Card, Alert } from "react-bootstrap";
 
 const Movies = () => {
-  const [moviesData, setMoviesData] = useState(null);
+  const [moviesData, setMoviesData] = useState<any[]>([]);
 
   useEffect(() => {
     popularMovies()
       .then((response) => {
-        setMoviesData(response.data);
+        setMoviesData(response.data.results);
       })
       .catch((error) => {
         console.error("Error fetching popular movies:", error);
       });
   }, []);
 
+  const card = moviesData.map((movie) => (
+    <div className="col-lg-3 col-md-4 col-sm-6 mb-3" key={movie.id}>
+      <Card style={{ width: "auto" }} className="h-100">
+        <div className="text-center">
+          <Card.Img
+            variant="top"
+            src={`https://www.themoviedb.org/t/p/original/${movie.backdrop_path}`}
+            className="mt-3"
+          />
+        </div>
+        <Card.Body>
+          <Card.Title>{movie.title}</Card.Title>
+        </Card.Body>
+        <Card.Footer className="bg-white">
+          <Button variant="primary">View</Button>
+        </Card.Footer>
+      </Card>
+    </div>
+  ));
+
   return (
     <>
-      <div>
-        {moviesData ? (
-          <pre>{JSON.stringify(moviesData, null, 2)}</pre>
+      <div className="row m-3">
+        {moviesData.length ? (
+          card
         ) : (
-          "Loading movies..."
+          <Alert variant="danger">No movies found!</Alert>
         )}
-      </div>
+      </div>{" "}
     </>
   );
 };
